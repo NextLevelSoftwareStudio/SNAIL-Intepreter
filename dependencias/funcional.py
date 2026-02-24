7f3ae9fb7fc56e3c3d03f35f4679f82cfb872349
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-02-24 21:48:43 +0000

    .

--- dependencias/funcional.py
+++ dependencias/funcional.py
@@ -1,23 +1,14 @@
 import memória, imports_, atalhos
 import re
 def código():
-    def extrair(texto, palavra):
-        padrao = rf"{re.escape(palavra)}(.*?)(?=(?://)|$)"
-        resultados = re.findall(padrao, texto, re.MULTILINE)
-        return [r.strip() for r in resultados]
     modo = memória.mem.ler(1)
     ficheiro = memória.mem.ler(2)
     bibliotecas = memória.mem.ler(3)
     try:
-        imports_.código()
         with open(ficheiro, 'r', encoding='utf-8') as file:
             conteúdo = file.read()
-        linhas = conteúdo.splitlines()
-        conteúdo_limpo = []
-        for num, linha in enumerate(linhas, start=1):
-            if not linha: continue
-            if not linha.endswith(';'):
-                print()
+        ficheiro_novo = conteúdo.replace(";", "")
+        imports_.código()
     except PermissionError as e:
         print(f"Error 4: Pemission error. ({e})")
         atalhos.sair(modo=0)
