e3e84cc0f72b8d2b0ed992df439bb3681e5a0506
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-02-27 10:45:53 +0000

    .

--- SNAIL 1.0.0/W/W11H/dependencias/funcional_web.py
+++ SNAIL 1.0.0/W/W11H/dependencias/funcional_web.py
@@ -1,8 +1,11 @@
 import memória
 from dependenciasWeb import verificador
+ficheiro = str(memória.mem.ler(id_escolhido=2))
 def código():
-    conteúdo_do_ficheiro = open(memória.mem.ler(id_escolhido=2), "r", encoding="utf-8")
-    novo = str(conteúdo_do_ficheiro).replace(";", "")
-    # Remover a primeira linha
-    novo2 = novo.replace(memória.mem.ler(id_escolhido=8), "")
+    with open(ficheiro, "r", encoding="utf-8") as file:
+        conteúdo_do_ficheiro = file.read()
+        novo = str(conteúdo_do_ficheiro).replace(";", "")
+        linhas = file.readlines()
+    # Arquivo sem a primeira linha
+    novo2 = linhas[1:]
     verificador.código()
