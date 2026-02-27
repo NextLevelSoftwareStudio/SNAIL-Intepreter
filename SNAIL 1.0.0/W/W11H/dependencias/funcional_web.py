8776ba43d3da9e61acd76c9b7dc35c890b469fb5
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-02-27 10:56:21 +0000

    .

--- SNAIL 1.0.0/W/W11H/dependencias/funcional_web.py
+++ SNAIL 1.0.0/W/W11H/dependencias/funcional_web.py
@@ -3,7 +3,6 @@ from dependenciasWeb import verificador
 ficheiro = str(memória.mem.ler(id_escolhido=2))
 def código():
     with open(ficheiro, "r", encoding="utf-8") as file:
-        conteúdo_do_ficheiro = file.read()
         linhas = file.readlines()
         novo = str(linhas).replace(";", "")
     novo2 = novo[1:]
