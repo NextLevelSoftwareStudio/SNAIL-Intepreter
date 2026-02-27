13916261230c6b9a1108bd7281d04597ba4d920d
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-02-27 10:51:59 +0000

    .

--- SNAIL 1.0.0/W/W11H/dependencias/funcional_web.py
+++ SNAIL 1.0.0/W/W11H/dependencias/funcional_web.py
@@ -4,8 +4,8 @@ ficheiro = str(memória.mem.ler(id_escolhido=2))
 def código():
     with open(ficheiro, "r", encoding="utf-8") as file:
         conteúdo_do_ficheiro = file.read()
-        novo = str(conteúdo_do_ficheiro).replace(";", "")
         linhas = file.readlines()
-    # Arquivo sem a primeira linha
-    novo2 = linhas[1:]
+        novo = str(linhas).replace(";", "")
+    novo2 = novo[1:]
+    memória.mem.guardar(id_escolhido=9, texto=novo2)
     verificador.código()
