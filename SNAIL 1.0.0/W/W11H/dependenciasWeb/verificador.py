2321b2a01c6bc80683ede0d3fc568a0704d3649a
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-02-27 12:46:03 +0000

    A list of languages has been added.

--- SNAIL 1.0.0/W/W11H/dependenciasWeb/verificador.py
+++ SNAIL 1.0.0/W/W11H/dependenciasWeb/verificador.py
@@ -1,3 +1,14 @@
-from dependencias import memória
+from dependencias import memória, atalhos
 def código():
-    conteúdo_do_ficheiro = open(memória.mem.ler(id_escolhido=2), "r", encoding="utf-8")
+    conteúdo_do_ficheiro = memória.mem.ler(id_escolhido=9)
+    ficheiro_html = """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Título</title>
+</head>
+<body>
+    </body>
+</html>"""
+    códigos_de_idiomas 
